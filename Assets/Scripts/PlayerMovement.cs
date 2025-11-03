11c95343363d63ff6abc102a1318192ad6c56d7c
Zander Goodwin <zanderngoodwin@gmail.com>
2025-11-03 12:21:54 -0400

    Halfway through Scene 3

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float step;
+    public GameObject childCube;
+    public GameObject enemy;
+    static float scalar = 1f;
+    private Vector3 startPos;
+    private Vector3 enemyDir;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+
+        childCube.transform.position = transform.position + transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) scalar += step;
+        if (Input.GetKeyDown(KeyCode.S)) scalar -= step;
+
+        transform.position = startPos * scalar;
+    }
+}
